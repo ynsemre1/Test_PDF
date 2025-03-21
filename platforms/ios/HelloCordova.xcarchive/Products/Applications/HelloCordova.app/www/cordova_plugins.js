582a620f9eba052d75f55ef1fdd7b098274ea428
Yunus Emre <kahraman03040506@hotmail.com>
2025-03-21 10:28:26 +0300

    iOS Build

---
+++ platforms/ios/HelloCordova.xcarchive/Products/Applications/HelloCordova.app/www/cordova_plugins.js
@@ -0,0 +1,75 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-camera.Camera",
+      "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "Camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverOptions",
+      "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverOptions"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.camera",
+      "file": "plugins/cordova-plugin-camera/www/Camera.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "navigator.camera"
+      ]
+    },
+    {
+      "id": "cordova-plugin-camera.CameraPopoverHandle",
+      "file": "plugins/cordova-plugin-camera/www/ios/CameraPopoverHandle.js",
+      "pluginId": "cordova-plugin-camera",
+      "clobbers": [
+        "CameraPopoverHandle"
+      ]
+    },
+    {
+      "id": "@red-mobile/cordova-plugin-barcodescanner.BarcodeScanner",
+      "file": "plugins/@red-mobile/cordova-plugin-barcodescanner/www/barcodescanner.js",
+      "pluginId": "@red-mobile/cordova-plugin-barcodescanner",
+      "clobbers": [
+        "cordova.plugins.barcodeScanner"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
+    },
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-dialogs.notification",
+      "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+      "pluginId": "cordova-plugin-dialogs",
+      "merges": [
+        "navigator.notification"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-camera": "8.0.0",
+    "@red-mobile/cordova-plugin-barcodescanner": "9.0.8",
+    "cordova-plugin-statusbar": "4.0.0",
+    "cordova-plugin-device": "3.0.0",
+    "cordova-plugin-dialogs": "2.0.2"
+  };
+});
